6263e69fe072903655fcca01cded1a53eb068cfd
Wang Yunfei <tszn1984@gmail.com>
2026-08-19 16:12:21 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -677,10 +677,14 @@ with tab_box:
         st.session_state[
             "boxplot_gene_text"
         ] = ",".join(default_genes)
+    else:
+        st.write("box_gene_text is empty")
+        
 
     gene_text = st.text_area(
         "Genes (comma separated)",
         height=120,
+        value=",".join(default_genes),
         key="boxplot_gene_text"
     )
     st.write(st.session_state["boxplot_gene_text"])
