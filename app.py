8abd8d70cc5295358e9ea73088f908499c2da680
Wang Yunfei <tszn1984@gmail.com>
2026-08-20 12:59:13 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -732,9 +732,10 @@ with tab_volcano:
                 "highlight_genes"
             ] = highlight_genes
         else:
+            highlight_genes = st.session_state.get("highlight_genes".[])
             st.session_state[
                 "highlight_genes"
-            ] = []            
+            ] = highlight_genes
         
         # --------------------------------------------------
         # Add Gene
